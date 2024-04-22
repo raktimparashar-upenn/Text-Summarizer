14a31c5f7fad84ca81e3f27f75ea3ff139521fb8
aarjayparashar <94872130+aarjayparashar@users.noreply.github.com>
2024-04-21 20:51:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome!")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
