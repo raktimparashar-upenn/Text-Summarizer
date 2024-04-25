bafd60dcfdcf28ea9f44985739110edaf8f3cb15
aarjayparashar <94872130+aarjayparashar@users.noreply.github.com>
2024-04-24 19:29:25 -0700

    Update prediction.py

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -7,13 +7,11 @@ class PredictionPipeline:
     def __init__(self):
         self.config = ConfigurationManager().get_model_evaluation_config()
 
-
-    
-    def predict(self,text):
+    def predict(self, text):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+        gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
 
-        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
+        pipe = pipeline("summarization", model=self.config.model_path, tokenizer=tokenizer)
 
         print("Dialogue:")
         print(text)
